15f714362254bd41bc5a354298045af607a9ad94
phamhoang2411 <phh24112006@gmail.com>
2026-06-22 00:37:26 +0700

    Commit đăng xuất

---
+++ Assets/_Script/LogOut.cs
@@ -0,0 +1,19 @@
+﻿using Firebase.Auth;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogOut : MonoBehaviour
+{
+    // Hàm gọi khi nhấn nút "Log Out"
+    public void LogOutButtonPressed()
+    {
+        // 1. Gọi lệnh đăng xuất của Firebase
+        FirebaseAuth.DefaultInstance.SignOut();
+
+        Debug.Log("Đã đăng xuất thành công!");
+
+        // 2. Chuyển hướng người dùng quay trở lại LoginScene
+        // Thay "LoginScene" bằng tên chính xác Scene đăng nhập của bạn
+        SceneManager.LoadScene(1);
+    }
+}
