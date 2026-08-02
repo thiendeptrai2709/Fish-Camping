1e5decc10635b7f707d91cc2cd109997516f8820
Huy đá <toitenhuy06@gmail.com>
2026-08-02 22:59:10 +0700

    done_shop popup

---
+++ Assets/_Script/AudioManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    [Header("Cài đặt Âm thanh UI")]
+    public AudioSource uiAudioSource; // Nguồn phát âm thanh
+    public AudioClip clickSound;      // File âm thanh (tiếng Tít/Click)
+
+    private void Awake()
+    {
+        // Khởi tạo Singleton
+        if (Instance == null)
+        {
+            Instance = this;
+            // DontDestroyOnLoad(gameObject); // Bỏ comment dòng này nếu muốn loa tổng xuyên qua các Scene
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Hàm này để các nút bấm gọi tới
+    public void PhatAmThanhClick()
+    {
+        if (uiAudioSource != null && clickSound != null)
+        {
+            // PlayOneShot giúp âm thanh click nhiều lần không bị đè hay ngắt quãng
+            uiAudioSource.PlayOneShot(clickSound);
+        }
+    }
+}
---
+++ Assets/_Script/UIButtonSound.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))] // Tự động yêu cầu object phải có UI Button
+public class UIButtonSound : MonoBehaviour
+{
+    private void Start()
+    {
+        Button nutBam = GetComponent<Button>();
+
+        if (nutBam != null)
+        {
+            // Tự động chèn lệnh gọi âm thanh vào sự kiện click của nút
+            nutBam.onClick.AddListener(() =>
+            {
+                if (AudioManager.Instance != null)
+                {
+                    AudioManager.Instance.PhatAmThanhClick();
+                }
+            });
+        }
+    }
+}
